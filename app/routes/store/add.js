0cb81c573e47f6f5eb113c915d1ac5a4b965a4ad
egodefroy <enzo.godefroy@sts-sio-caen.info>
2021-03-29 17:06:16 +0200

    gestion store add section

--- app/routes/store/add.js
+++ app/routes/store/add.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
 export default class StoreAddRoute extends Route {
+  model() {
+    return {};
+  }
+
+  @action save(sectionData) {
+    let section = this.store.createRecord('section', sectionData);
+    section.save().then(() => {
+      this.transitionTo('store');
+    });
+  }
 }
