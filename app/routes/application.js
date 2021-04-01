611c57e46847f6ff869597f8b6d875506c4db01e
spotentier <sam.potentier@sts-sio-caen.info>
2021-04-01 16:39:48 +0200

    connection disconnection

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,16 @@
-import RSVP from "rsvp";
-import AbstractRouteRoute from "./abstract-route";
+import RSVP from 'rsvp';
+import { inject as service } from '@ember/service';
+import AbstractRouteRoute from './abstract-route';
+import { action } from '@ember/object';
 
 export default class ApplicationRoute extends AbstractRouteRoute {
+  @service userAuth;
   model() {
     return RSVP.hash({
       connected: this.userAuth.user,
     });
+  }
+  @action logout() {
+    this.userAuth.logout();
   }
 }
