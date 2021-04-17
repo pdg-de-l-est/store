0ea577c99d0b4c46c97215e4dd6c43901308e25f
Enzo <enzo.godefroy@sts-sio-caen.info>
2021-04-17 15:37:25 +0200

    gestion store debut edit pack https://github.com/pdg-de-l-est/store/issues/39

--- app/router.js
+++ app/router.js
@@ -15,16 +15,17 @@ Router.map(function () {
     this.route('update', { path: 'update/:product_id' });
   });
   this.route('store', function () {
-    this.route('detail', { path: 'detail/:section_id' }, function() {
+    this.route('detail', { path: 'detail/:section_id' }, function () {
       this.route('add-product', { path: 'add-product/:section_id' });
       this.route('edit-product', { path: 'edit-product/:product_id' });
+      this.route('edit-pack', { path: 'edit-pack/:pack_id' });
     });
-    this.route('index',function() {
+    this.route('index', function () {
       this.route('add');
       this.route('edit', { path: 'edit/:section_id' });
       this.route('add-pack');
       this.route('delete-section', { path: 'delete-section/:section_id' });
-    })
+    });
   });
   this.route('gestionStock', function () {
     this.route('section', { path: 'section/:section_id' });
