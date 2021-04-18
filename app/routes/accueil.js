9f3644797880f26ad5d8881caa53b4cf6bef3b07
Enzo <enzo.godefroy@sts-sio-caen.info>
2021-04-18 15:54:53 +0200

    page accueil

--- app/routes/accueil.js
+++ app/routes/accueil.js
@@ -1,4 +1,11 @@
-import Route from '@ember/routing/route';
+import AbstractRouteRoute from "./abstract-route";
+import RSVP from "rsvp";
 
-export default class AccueilRoute extends Route {
+export default class AccueilRoute extends AbstractRouteRoute {
+  model(){
+    return RSVP.hash({
+      employees: this.store.findAll('employee', { include: 'orders' }),
+      connected: this.userAuth.user,
+    });
+  }
 }
--- app/routes/application.js
+++ app/routes/application.js
@@ -12,5 +12,6 @@ export default class ApplicationRoute extends AbstractRouteRoute {
   }
   @action logout() {
     this.userAuth.logout();
+    this.transitionTo('application');
   }
 }
