7c1fdaab13aa9e2fdfa1dbdbbf595e1bd7deab87
PotentierSam <sam.potentier@sts-sio-caen.info>
2021-04-21 11:35:17 +0200

    gestion employee start

--- app/models/order.js
+++ app/models/order.js
@@ -4,6 +4,7 @@ export default class OrderModel extends Model {
   @attr('string') dateCreation;
   @attr('string') status;
   @attr('number') itemsNumber;
+  @attr('number') amount;
   @belongsTo('user') user;
   @belongsTo('employee') employee;
   @hasMany('orderdetail', { inverse: null }) orderdetails;
