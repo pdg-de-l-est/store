b5b3221a546330f5afaf5ab5e00e352b9b6ac480
Thomas Deniaux <thomas.deniaux@sts-sio-caen.info>
2021-04-21 15:23:07 +0200

    Marquer tous les produits comme préparé

--- app/routes/dashboard/detail-commande.js
+++ app/routes/dashboard/detail-commande.js
@@ -22,4 +22,31 @@ export default class DashboardDetailCommandeRoute extends AbstractRouteRoute {
       this.transitionTo('dashboard');
     });
   }
+
+  @action prepareAll(order){
+    //initalisation des compteurs
+    let len = 0;
+    let nbPrepared = 0;
+    //Première boucle pour incrémenter les compteurs
+    order.orderdetails.forEach(element => {
+      len = len+1;
+      if( element.prepared == 1){
+        nbPrepared = nbPrepared+1;
+      }
+    });
+    //Si tous les orderdetails sont préparés alors on les "déprépare"
+   if(len == nbPrepared){
+        order.orderdetails.forEach(element => {
+          element.prepared = 0;
+          this.save(element);
+      });
+    }
+    //Sinon on les prépare tous
+    else{
+      order.orderdetails.forEach(element => {
+        element.prepared = 1;
+        this.save(element);
+    });
+  }
+ }
 }
