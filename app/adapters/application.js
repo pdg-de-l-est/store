ad858deb9144c52e82eac830467e149726be120f
Thomas Deniaux <thomas.deniaux@sts-sio-caen.info>
2021-05-23 17:09:34 +0200

    Build2

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
 import RESTAdapter from '@ember-data/adapter/rest';
 
 export default class ApplicationAdapter extends RESTAdapter {
-  host = ' https://pdg-est.sts-sio-caen.info/rest';
+  host = ' http://pdg-est.sts-sio-caen.info/rest';
 }
