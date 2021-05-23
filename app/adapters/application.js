0417346768f0451c9d43ad7197aff9852f34ec0b
Thomas Deniaux <thomas.deniaux@sts-sio-caen.info>
2021-05-23 18:04:44 +0200

    ajout du s à http

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
 import RESTAdapter from '@ember-data/adapter/rest';
 
 export default class ApplicationAdapter extends RESTAdapter {
-  host = ' http://pdg-est-1.sts-sio-caen.info/rest';
+  host = ' https://pdg-est-1.sts-sio-caen.info/rest';
 }
--- dist/assets/store.js
+++ dist/assets/store.js
@@ -29,7 +29,7 @@
     constructor(...args) {
       super(...args);
 
-      _defineProperty(this, "host", ' http://pdg-est-1.sts-sio-caen.info/rest');
+      _defineProperty(this, "host", ' https://pdg-est-1.sts-sio-caen.info/rest');
     }
 
   }
@@ -2976,7 +2976,7 @@ catch(err) {
 
 ;
           if (!runningTests) {
-            require("store/app")["default"].create({"name":"store","version":"0.0.0+f8412880"});
+            require("store/app")["default"].create({"name":"store","version":"0.0.0+de55d4d8"});
           }
         
 //# sourceMappingURL=store.map
